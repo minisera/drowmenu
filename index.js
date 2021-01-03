d11889115c6a78d492697677f081dc96aa811387
minisera <koki.ohtaka0821@gmail.com>
2021-01-03 17:48:26 +0900

    feat: メニュー出るよう変更

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-$(function() {
-  $('.hamburger').click(function() {
-      $(this).toggleClass('active');
+// $(function() {
+//   $('.hamburger').click(function() {
+//       $(this).toggleClass('active');
 
-      if ($(this).hasClass('active')) {
-          $('.globalMenuSp').addClass('active');
-      } else {
-          $('.globalMenuSp').removeClass('active');
-      }
-  });
+//       if ($(this).hasClass('active')) {
+//           $('.globalMenuSp').addClass('active');
+//       } else {
+//           $('.globalMenuSp').removeClass('active');
+//       }
+//   });
+// });
+console.log("こんにちは");
+const hamburger = document.querySelector('.hamburger');
+const glob = document.querySelector('.globalMenuSp');
+
+hamburger.addEventListener("click",()=>{
+  // console.log(hamburger);
+  hamburger.classList.toggle("active");
+
+  if (hamburger.classList.contains("active")){
+    glob.classList.add("active");
+  }
+  else {
+    glob.classList.remove("active");
+    }
 });
